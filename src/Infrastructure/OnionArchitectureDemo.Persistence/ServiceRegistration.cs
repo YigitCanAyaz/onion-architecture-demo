602f6d53138bb0a984508e98d01a56ecad4669bb
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2023-01-12 10:40:16 +0300

    Add project files.

---
+++ src/Infrastructure/OnionArchitectureDemo.Persistence/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchitectureDemo.Application.Interfaces.Repository;
+using OnionArchitectureDemo.Persistence.Context;
+using OnionArchitectureDemo.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitectureDemo.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("memoryDb"));
+
+            // Registration
+            serviceCollection.AddTransient<IProductRepository, ProductRepository>();
+        }
+    }
+}
---
+++ src/WebApi/OnionArchitectureDemo.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionArchitectureDemo.Application.Dto;
+using OnionArchitectureDemo.Application.Interfaces.Repository;
+
+namespace OnionArchitectureDemo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allList = await _productRepository.GetAllAsync();
+
+            var result = allList.Select(i => new ProductViewDto()
+            {
+                Id = i.Id,
+                Name = i.Name,
+            }).ToList();
+
+            return Ok(result);
+        }
+    }
+}
